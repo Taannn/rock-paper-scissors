8ffe1e17471d3a7f93d87be75390866ee09fe5a2
Tristan <142246753+Taannn@users.noreply.github.com>
2023-10-23 16:32:41 +0800

    added boilderplate

---
+++ script.js
@@ -0,0 +1,6 @@
+const computerChoice = document.getElementById('computer-choice');
+const userChoice = document.getElementById('player-choice');
+const displayResult = document.getElementById('result');
+
+const possibleChoices = document.querySelectorAll('button');
+
