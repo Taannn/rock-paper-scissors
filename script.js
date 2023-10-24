f55b6dc15e3b35d4b3f95934d4a9002716391c9f
Tristan <142246753+Taannn@users.noreply.github.com>
2023-10-24 13:45:21 +0800

    savepoint-number1
    adding the user choices

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const computerChoice = document.getElementById('computer-choice');
-const userChoice = document.getElementById('player-choice');
+//picking the elements
+const displayComputerChoice = document.getElementById('computer-choice');
+const displayUserChoice = document.getElementById('user-choice');
 const displayResult = document.getElementById('result');
-
+// get all the possible choices
 const possibleChoices = document.querySelectorAll('button');
+//
+let userChoice;
 
+// grabbing the button                                                        e for event
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+  // whenever I click, it gets saved to userChoice
+  userChoice = e.target.id;
+  //
+  displayUserChoice.innerHTML = userChoice;
+}));
